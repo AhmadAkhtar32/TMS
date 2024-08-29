7a9447cdbe377e3810603d9b0e0f4ead14eece58
Ahmad Akhtar <147875473+AhmadAkhtar32@users.noreply.github.com>
2024-08-29 21:29:04 +0500

    Update Login.java

--- src/travel/management/system/Login.java
+++ src/travel/management/system/Login.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Login File
  */
 //User name = ahmad , password = 123456
 package travel.management.system;
